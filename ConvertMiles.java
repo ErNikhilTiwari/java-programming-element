1e96aecfd42ea83899fac5db91357a595bc53882
ErNikhilTiwari <102575456+ErNikhilTiwari@users.noreply.github.com>
2025-03-02 20:49:04 +0530

    deleted all files

--- ConvertMiles.java
+++
@@ -1,7 +0,0 @@
-public class ConvertMiles{
-	public static void main(String[] args){
-	double distanceKm= 10.8;
-	double distanceMiles = 10.8 * 1.6;
-	System.out.print("The distance "+distanceKm +" km in miles is "+distanceMiles);
-	}
-}
